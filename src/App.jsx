ba9606bb8c773b8266a3d31ba136b70797eae2e6
steven huynh <steven.h@live.com>
2024-05-09 20:48:51 -0700

    implement image download

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import Option from "./Option"
 import accessories from "./options"
+import html2canvas from "html2canvas";
 
 function App() {
+  const anchorRef = useRef(null);
+  const alpacaRef = useRef(null);
   const [focus, setFocus] = useState('');
   const [style, setStyle] = useState('');
   const [image, setImage] = useState({
-    'accessories': '',
+    'accessories': 'earings',
     'backgrounds': 'blue50',
     'ears': 'default',
     'eyes': 'default',
@@ -41,11 +44,25 @@ function App() {
     })
   }
 
+  function download() {
+    (async () => {
+      if (alpacaRef.current && anchorRef.current) {
+        let canvas = await html2canvas(alpacaRef.current, {
+          windowWidth: alpacaRef.current.scrollWidth,
+          windowHeight: alpacaRef.current.scrollHeight
+        });
+        let image = canvas.toDataURL('image/png');
+        anchorRef.current.href = image;
+        anchorRef.current.click();
+      }
+    })();
+  }
+
   return (
     <div className='container mx-auto h-screen bg-gray-300'>
       <h1 className="uppercase text-3xl text-center p-9">alpaca generator</h1>
       <div className="flex justify-center gap-6">
-        <div className="canvas w-96 relative flex-none">
+        <div className="w-96 h-96 relative flex-none" ref={alpacaRef}>
           <img src={`/public/assets/alpaca/backgrounds/${image['backgrounds']}.png`} alt="" className="absolute" />
           <img src={`/public/assets/alpaca/accessories/${image['accessories']}.png`} alt="" className="absolute z-40" />
           <img src={`/public/assets/alpaca/ears/${image['ears']}.png`} alt="" className="absolute" />
@@ -78,10 +95,14 @@ function App() {
               onClick={() => randomize()}>
               Randomize
             </button>
-            <button className="bg-blue-400 rounded-lg p-2 text-white hover:bg-blue-500 focus:bg-blue-700">Download</button>
+            <button className="bg-blue-400 rounded-lg p-2 text-white hover:bg-blue-500 focus:bg-blue-700"
+              onClick={() => download()}>
+              Download
+            </button>
           </div>
         </div>
       </div>
+      <a ref={anchorRef} download='alpaca' className="hidden"></a>
     </div>
   )
 }
